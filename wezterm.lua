a72220af162ebae66a2135ea4c800e55e5a950cf
Benson Chu <bensonchu457@gmail.com>
2024-05-31 21:13:39 -0500

    More keys

--- wezterm.lua
+++ wezterm.lua
@@ -7,6 +7,8 @@ local config = {}
 -- Use config builder object if possible
 if wezterm.config_builder then config = wezterm.config_builder() end
 
+config.debug_key_events = true
+
 config.font = wezterm.font_with_fallback({
     { family = "Roboto Mono", scale = 0.8 },
     { family = "FiraCode Nerd Font Mono", scale = 0.9 }
@@ -17,6 +19,7 @@ config.keys = {
     { key = ',', mods = 'CTRL', action = emacs_keys.SendEmacs("C-,") },
     { key = ' ', mods = 'CTRL', action = emacs_keys.SendEmacs("C-SPC") },
     { key = '/', mods = 'CTRL', action = emacs_keys.SendEmacs("C-/") },
+    { key = 'Backspace', mods = 'CTRL', action = emacs_keys.SendEmacs("C-<backspace>") },
 }
 
 return config
